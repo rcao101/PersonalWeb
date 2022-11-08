0008189077d274e9afbdc7a60758fafb6c141bb4
ruby <rubycao04@gmail.com>
2022-11-08 11:44:16 -0800

    created courselist constraint and made the each array a list

---
+++ scripts/scripts.js
@@ -0,0 +1,28 @@
+const courselist = ["ACIT 1420", "ACIT 1620", "COMM 1116"]
+
+
+Let Acit 1420 = {
+    name: "System Administrators",
+    code: "1420",
+    program: "CIT",
+}
+
+Let ACIT 1620 = {
+    name: "Web Fundamental Technologies",
+    code: "1620",
+    program: "CIT",
+}
+
+Let COMM 1116 = {
+    name: "System Administrators",
+    code: "1420",
+    program: "CIT",
+}
+
+
+let input = ""
+do {
+    input = prompt ("Enter a 4 digit number");
+}
+
+while ()
